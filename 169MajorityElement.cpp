46885356ad6bbea8e129d55a2fc067844fa6482b
anshikarawat07 <160812154+anshikarawat07@users.noreply.github.com>
2024-09-28 19:42:25 +0530

    Update 169MajorityElement.cpp

--- 169MajorityElement.cpp
+++ 169MajorityElement.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count_of_value;
-        for(int value:nums)
-        {
-         count_of_value = count(nums.begin(), nums.end(), value);
-         if(count_of_value>=float(nums.size())/2)
-            return value;
+        int freq = 0, ans = 0;
+        for(int i=0; i<nums.size(); i++) {
+        if(freq == 0) 
+             ans = nums [i] ;
+        if(ans == nums [i]) 
+            freq++;
+         else 
+            freq --;
     }  
-    return 0 ;
+    return ans ;
     }
 };
